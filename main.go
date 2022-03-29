a22e77af7652e977d24da22c96cce954cbfd2359
Yohann <yohann@blueboard.io>
2022-03-29 14:56:29 +0200

    cut func

--- main.go
+++ main.go
@@ -6,10 +6,24 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/gocolly/colly"
 )
 
-func main() {
+func testScrape() {
+	fmt.Println("HELLO")
+	c := colly.NewCollector()
 
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println(r.StatusCode)
+	})
+	c.Visit("https://www.blueboard.io")
+}
+
+func scrapeIPInfo() {
 	pURL, _ := url.Parse(`http://85.25.198.20:5566`)
 	httpClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(pURL)}}
 	resp, err := httpClient.Get("https://www.ipinfo.io")
@@ -22,5 +36,9 @@ func main() {
 		log.Fatalln(err)
 	}
 	fmt.Println(string(b))
+}
 
+func main() {
+	testScrape()
+	// scrapeIPInfo()
 }
