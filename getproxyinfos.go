f190a07a82d741a97dda8c877e8691c5c2f924b1
badakzz <lucas.deray@gmail.com>
2022-04-02 03:55:38 +0200

    proxynova into a .txt file + proxy tester

---
+++ getproxyinfos.go
@@ -0,0 +1,22 @@
+package main
+
+// func getProxyInfos()
+// {
+
+// func main() {
+// 	file, err := os.Open("~/NeoChromeViewVerse/data.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer file.Close()
+
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		fmt.Println(scanner.Text())
+
+// 	}
+
+// 	if err := scanner.Err(); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
