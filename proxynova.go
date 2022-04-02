2e495bab420c5df127a07fc11690a9f9a5dd3a5d
badakzz <lucas.deray@gmail.com>
2022-04-02 03:57:33 +0200

    forgot proxynova

--- proxynova.go
+++ proxynova.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 func scrapeProxynova() {
+	f, err := os.Create("data.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
 
 	c := colly.NewCollector()
 	c.OnRequest(func(r *colly.Request) {
@@ -18,14 +27,25 @@ func scrapeProxynova() {
 		fmt.Println(r.StatusCode)
 	})
 
-	i := 0
+	i := -1
+	y := 0
+	j := 0
 	var ipAddress string
+
 	c.OnHTML("tbody", func(e *colly.HTMLElement) {
 		e.ForEach("tr", func(_ int, trElem *colly.HTMLElement) {
+			y++
+		})
+	})
+
+	c.OnHTML("tbody", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(_ int, trElem *colly.HTMLElement) {
+			j++
 			i = 0
 			trElem.ForEachWithBreak("td", func(_ int, tdElem *colly.HTMLElement) bool {
 				if !strings.Contains(tdElem.Text, "google") {
 					i++
+
 					if i == 1 {
 						ipAddress = getStringBetween(tdElem.Text, `(`, `)`)
 						ipAddress = strings.ReplaceAll(ipAddress, " ", "")
@@ -34,16 +54,40 @@ func scrapeProxynova() {
 						ipAddress = strings.ReplaceAll(ipAddress, `"`, "")
 						ipAddress += ":"
 					}
-					if i == 2 {
+
+					if i == 2 && j != (y-1) {
 						ipAddress += (strings.ReplaceAll(strings.ReplaceAll(tdElem.Text, " ", ""), "\n", ""))
-						fmt.Println(ipAddress)
+						n, err := fmt.Fprintln(f, ipAddress)
+
+						if err != nil {
+
+							log.Fatal(err)
+						}
+
+						fmt.Println(n, "bytes written")
+						fmt.Println("done")
+						return false
+					} else if i == 2 && j == (y-1) {
+						ipAddress += (strings.ReplaceAll(strings.ReplaceAll(tdElem.Text, " ", ""), "\n", ""))
+						n, err := fmt.Fprint(f, ipAddress)
+
+						if err != nil {
+
+							log.Fatal(err)
+						}
+
+						fmt.Println(n, "bytes written")
+						fmt.Println("done")
 						return false
 					}
+
 				}
 				return true
 			})
 		})
-	})
 
+	})
+	fmt.Println(y)
 	c.Visit("https://proxynova.com/proxy-server-list/country-fr")
+
 }
