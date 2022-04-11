fff55676d6eefc55e6ce26a9e84c35a99d49d99b
badakzz <lucas.deray@gmail.com>
2022-04-11 17:23:16 +0200

    fixed whitelister erasing first line of the output file

--- proxywhitelister.go
+++ proxywhitelister.go
@@ -7,25 +7,28 @@ import (
 	"os"
 )
 
-func proxyWhiteLister() {
-	file, err := os.Open("/home/badakzz/NeoChromeViewVerse/data.txt")
+func proxyWhiteLister(inputPath string, outputPath string) {
+	// file, err := os.Open("/home/badakzz/NeoChromeViewVerse/data.txt")
+	file, err := os.Open(inputPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+	f, err := os.Create(outputPath)
 
+	if err != nil {
+		log.Fatal(err)
+	}
 	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 		str, ok := scrapeIPInfo(scanner.Text())
+
+		defer f.Close()
+
 		if !ok {
-			f, err := os.Create("pogggg.txt")
 
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			defer f.Close()
 			_, err = fmt.Fprintln(f, str)
 
 			if err != nil {
